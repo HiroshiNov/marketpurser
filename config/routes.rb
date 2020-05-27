db459477e1ddb49c01fe9c7aa5760cf104a06bb2
hiroshi.m <hiroshi.m.nov@gmail.com>
2020-05-27 15:32:17 +0000

    sessions controller, sessions new

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   root to: "toppages#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "setteings", to: "users#edit"
   get "dashborad", to: "users#show"
   get "signup", to: "users#new"
+
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
 
   resources :users, only: [:show,:new,:create,:edit,:update,:destroy]
 end
