262a02e921973dbe403a053c8cdcdef19be4942a
hiroshi.m <hiroshi.m.nov@gmail.com>
2020-05-30 01:14:35 +0000

    add dayjs cdn, modify chart.js

--- app/assets/javascripts/chart.js
+++ app/assets/javascripts/chart.js
@@ -1,21 +1,55 @@
 $(document).ready(function() {
+
+
   var data_json = $('.arr').val();
   var data = JSON.parse(data_json);
   var options = {
-    chart: {
-      height: 380,
-      width: "100%",
-      type: 'candlestick'
-    },
     series: [{
     data: data
     }],
+    // chart: {
+    //   height: 380,
+    //   width: "100%",
+    //   type: 'candlestick'
+    // },
+    // xaxis: {
+    // type: 'numeric',
+    //       labels: {
+    //         formatter: function(val) {
+    //           return dayjs(val).format('MMM DD YYYY')
+    //         }
+    //       }
+    //     }
+    chart: {
+      height: 350,
+      width:"100%",
+      type: 'candlestick',
+    },
+    title: {
+      text: 'CandleStick Chart - Category X-axis',
+      align: 'left'
+    },
+    tooltip: {
+      enabled: true,
+    },
     xaxis: {
-    type: 'numeric'
+      type: 'category',
+      labels: {
+            formatter: function(val) {
+              return dayjs.unix(val).format('MMM DD YY')
+            }
+      }
+    },
+    yaxis: {
+      tooltip: {
+            enabled: true
+      }
     }
-  }
+  };
+  
 
   var chart = new ApexCharts(document.querySelector("#chart"), options);
 
   chart.render();
 });
+
