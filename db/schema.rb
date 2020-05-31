cd3eec8b0e4601f9b95d8133fbe4b752c2c47e38
hiroshi.m <hiroshi.m.nov@gmail.com>
2020-05-31 09:35:05 +0000

    make it better

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_30_012141) do
+ActiveRecord::Schema.define(version: 2020_05_31_092607) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "content"
+    t.bigint "stock_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["stock_id"], name: "index_comments_on_stock_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "stocks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "symbol"
@@ -29,4 +39,6 @@ ActiveRecord::Schema.define(version: 2020_05_30_012141) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "stocks"
+  add_foreign_key "comments", "users"
 end
