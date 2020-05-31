909992b7eb5ce934625fc41cb325a0268785cae6
hiroshi.m <hiroshi.m.nov@gmail.com>
2020-05-31 13:25:15 +0000

    make it better

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
 before_action :require_user_logged_in
+before_action :correct_user, only: [:destroy]
 
   def create
     stock_insatnce = Stock.find(params[:stock_id])
@@ -14,9 +15,19 @@ before_action :require_user_logged_in
   #   flash[:success] = 'コメントを保存しました'
   #   redirect_to stock_insatnce
   # end
-
+  def destroy
+    @comment.destroy
+    flash[:success] = 'メッセージを削除しました。'
+    redirect_back(fallback_location: root_path)
+  end
   private
   def comment_params
     params.require(:comment).permit(:content)
+  end
+  def correct_user
+  @comment = current_user.comments.find_by(id: params[:id])
+  unless @comment
+    redirect_to root_url
+  end
   end
 end
